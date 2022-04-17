671b22aa9d4295b139d98416de4ac3c5dbafd268
Keshav Rathinavel <73035121+keshavrathinavel@users.noreply.github.com>
2022-04-17 15:41:14 +0530

    Update heart_code.py

--- heart_code.py
+++ heart_code.py
@@ -7,52 +7,63 @@ simplefilter(action='ignore', category = FutureWarning)
 from warnings import simplefilter
 # ignore all future warnings
 simplefilter(action='ignore', category = FutureWarning)
+import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-#reading dataset
-#IF PATH ERROR IS ENCOUNTERED, USE COMMAND PROMPT TO RUN CODE OR VS CODE TERMINAL
-df = pd.read_csv('dataset.csv', header = None)
-
-df.columns = ['age', 'sex', 'cp', 'trestbps', 'chol',
+# reading dataset
+# IF PATH ERROR IS ENCOUNTERED, USE COMMAND PROMPT TO RUN CODE OR VS CODE TERMINAL
+heart_data = pd.read_csv(r"..\Heart-Disease-Prediction-master\dataset.csv")
+heart_data.columns = ['age', 'sex', 'cp', 'trestbps', 'chol',
               'fbs', 'restecg', 'thalach', 'exang', 
               'oldpeak', 'slope', 'ca', 'thal', 'target']
 
 #Detecting empty values
-df.isnull().sum()
+heart_data.isnull().sum()
 
 #0 = female, 1 = male
-df['target'] = df.target.map({0: 0, 1: 1, 2: 1, 3: 1, 4: 1})
-df['sex'] = df.sex.map({0: 'female', 1: 'male'})
-df['thal'] = df.thal.fillna(df.thal.mean())
-df['ca'] = df.ca.fillna(df.ca.mean())
+heart_data['target'] = heart_data.target.map({0: 0, 1: 1, 2: 1, 3: 1, 4: 1})
+heart_data['sex'] = heart_data.sex.map({0: 'female', 1: 'male'})
+heart_data['thal'] = heart_data.thal.fillna(heart_data.thal.mean())
+heart_data['ca'] = heart_data.ca.fillna(heart_data.ca.mean())
 
-#importing library for plotting data
+# importing library for plotting data
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# data shape and info
+heart_data.shape
+heart_data.info()
+heart_data.describe()
+
+
 # distribution of target vs age 
 sns.set_context("paper", font_scale = 2, rc = {"font.size": 20,"axes.titlesize": 25,"axes.labelsize": 20}) 
-sns.catplot(kind = 'count', data = df, x = 'age', hue = 'target', order = df['age'].sort_values().unique())
+sns.catplot(kind = 'count', data = heart_data, x = 'age', hue = 'target', order = heart_data['age'].sort_values().unique())
 plt.title('Variation of Age for each target class')
 plt.show()
 
  
 # barplot of age vs sex with hue = target
-sns.catplot(kind = 'bar', data = df, y = 'age', x = 'sex', hue = 'target')
+sns.catplot(kind = 'bar', data = heart_data, y = 'age', x = 'sex', hue = 'target')
 plt.title('Distribution of age vs sex with the target class')
 plt.show()
 
-df['sex'] = df.sex.map({'female': 0, 'male': 1})
+heart_data['sex'] = heart_data.sex.map({'female': 0, 'male': 1})
 
+
+# data heatmap - check correlation between features
+plt.figure(figsize=(20,12))
+sns.set_context('notebook', font_scale=1.3)
+sns.heatmap(heart_data.corr(), annot=True, linewidth=2)
+plt.tight_layout()
+plt.show()
 
 # data preprocessing
-
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
-
-
+X = heart_data.iloc[:, :-1].values
+y = heart_data.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
 from sklearn.preprocessing import StandardScaler as ss
 sc = ss()
@@ -67,11 +78,13 @@ classifier.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-from sklearn.metrics import confusion_matrix
+
 cm_test = confusion_matrix(y_pred, y_test)
+print(cm_test)
 
 y_pred_train = classifier.predict(X_train)
 cm_train = confusion_matrix(y_pred_train, y_train)
+print(cm_train)
 
 print()
 print('Accuracy for training set for svm = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
@@ -79,11 +92,8 @@ print('Accuracy for test set for svm = {}'.format((cm_test[0][0] + cm_test[1][1]
 
 
 #########################################   Naive Bayes  #############################################################
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
@@ -93,23 +103,21 @@ classifier.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-from sklearn.metrics import confusion_matrix
+
 cm_test = confusion_matrix(y_pred, y_test)
+print(cm_test)
 
 y_pred_train = classifier.predict(X_train)
 cm_train = confusion_matrix(y_pred_train, y_train)
+print(cm_train)
 
 print()
 print('Accuracy for training set for Naive Bayes = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
 print('Accuracy for test set for Naive Bayes = {}'.format((cm_test[0][0] + cm_test[1][1])/len(y_test)))
 
 
-#########################################   Logistic Regression  #############################################################
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
+#########################################   Logistic Regression  #####################################################
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression()
@@ -118,22 +126,20 @@ classifier.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-from sklearn.metrics import confusion_matrix
+
 cm_test = confusion_matrix(y_pred, y_test)
+print(cm_test)
 
 y_pred_train = classifier.predict(X_train)
 cm_train = confusion_matrix(y_pred_train, y_train)
+print(cm_train)
 
 print()
 print('Accuracy for training set for Logistic Regression = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
 print('Accuracy for test set for Logistic Regression = {}'.format((cm_test[0][0] + cm_test[1][1])/len(y_test)))
 
-#########################################   Decision Tree  #############################################################
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
+#########################################   Decision Tree  ###########################################################
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 from sklearn.tree import DecisionTreeClassifier
 classifier = DecisionTreeClassifier()
@@ -142,23 +148,21 @@ classifier.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-from sklearn.metrics import confusion_matrix
+
 cm_test = confusion_matrix(y_pred, y_test)
+print(cm_test)
 
 y_pred_train = classifier.predict(X_train)
 cm_train = confusion_matrix(y_pred_train, y_train)
+print(cm_train)
 
 print()
 print('Accuracy for training set for Decision Tree = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
 print('Accuracy for test set for Decision Tree = {}'.format((cm_test[0][0] + cm_test[1][1])/len(y_test)))
 
 
-#########################################  Random Forest  #############################################################
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
+#########################################  Random Forest  ###########################################################
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 10)
@@ -167,14 +171,86 @@ classifier.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-from sklearn.metrics import confusion_matrix
+
 cm_test = confusion_matrix(y_pred, y_test)
+print(cm_test)
 
 y_pred_train = classifier.predict(X_train)
 cm_train = confusion_matrix(y_pred_train, y_train)
+print(cm_train)
 
 print()
 print('Accuracy for training set for Random Forest = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
 print('Accuracy for test set for Random Forest = {}'.format((cm_test[0][0] + cm_test[1][1])/len(y_test)))
 
-###############################################################################
+
+#################################################  XGBoost  #########################################################
+
+from xgboost import XGBClassifier
+classifier = XGBClassifier(random_state = 42)
+classifier.fit(X_train, y_train)
+
+# Predicting the test results
+y_pred = classifier.predict(X_test)
+
+cm_test = confusion_matrix(y_pred, y_test)
+print(cm_test)
+
+y_pred_train = classifier.predict(X_train)
+cm_train = confusion_matrix(y_pred_train, y_train)
+print(cm_train)
+
+print()
+print('Accuracy for training set for XGBoost = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
+print('Accuracy for test set for XGBoost = {}'.format((cm_test[0][0] + cm_test[1][1])/len(y_test)))
+
+
+#################################################  K-Nearest Neighbours  ############################################
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 10)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+cm_test = confusion_matrix(y_pred, y_test)
+print(cm_test)
+
+y_pred_train = classifier.predict(X_train)
+cm_train = confusion_matrix(y_pred_train, y_train)
+print(cm_train)
+
+print()
+print('Accuracy for training set for KNN = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
+print('Accuracy for test set for KNN = {}'.format((cm_test[0][0] + cm_test[1][1])/len(y_test)))
+
+# Hyperparameter Optimization
+
+test_score = []
+neighbors = range(1, 25)
+
+for k in neighbors:
+    model = KNeighborsClassifier(n_neighbors=k)
+    model.fit(X_train, y_train)
+    test_score.append(accuracy_score(y_test, model.predict(X_test)))
+
+plt.figure(figsize=(18, 8))
+plt.plot(neighbors, test_score, label="Test score")
+plt.xticks(np.arange(1, 21, 1))
+plt.xlabel("Number of neighbors")
+plt.ylabel("Model score")
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+knn = KNeighborsClassifier(n_neighbors = 5)
+knn.fit(X_train,y_train)
+y_pred = knn.predict(X_test)
+print("Accuracy for test set for KNN after hyperparameter optimization (n=5): \n", accuracy_score(y_test,y_pred))
+
+knn = KNeighborsClassifier(n_neighbors = 11)
+knn.fit(X_train,y_train)
+y_pred = knn.predict(X_test)
+print("Accuracy for test set for KNN after hyperparameter optimization (n=11): \n", accuracy_score(y_test,y_pred))
+
+#####################################################################################################################
